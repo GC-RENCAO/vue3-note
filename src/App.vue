25f109292c8ca100bfcef5ab8806b19597d35a42
YQHP-YuKi <yqhp.yuki@gmail.com>
2024-12-24 18:18:48 +0900

    vue learning day1

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- html结构 -->
+     <div class="app">
+        <h1>hello vue</h1>
+     </div>
+</template>
+
+<script lang="ts">
+    // JS或者TS
+    export default {
+        name: 'App' //组件名字
+    }
+</script>
+
+<style>
+    /* 样式 */  
+    .app {
+        background-color: bisque;
+        box-shadow: 0 0 10px;
+    }
+</style>
